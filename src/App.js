292d6d0fb54eba1174a3e006389a09d54911abbc
Mikhail Prytychenko <nightsta1kerhf@gmail.com>
2021-06-23 21:18:04 +0300

    v.3.0.3

--- src/App.js
+++ src/App.js
@@ -19,6 +19,22 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    console.log(parsedContacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contact) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   formSubmitHandler = contact => {
     const { contacts } = this.state;
     const { name } = contact;
